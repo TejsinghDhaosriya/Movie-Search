b22eba9e666db2126e57e6ef75a9ae21840e691a
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2021-02-26 22:21:12 +0530

    [FE]-added dark mode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+import { useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+
+import { withTheme }  from "./theme"
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    height: '100%',
+    [theme.breakpoints.down('xs')]: {
+      paddingTop: theme.spacing(2), // Material UI spacing
+    },
+    position: 'absolute',
+    overflow: 'auto',
+    backgroundColor: theme.palette.background.paper, // Makes it the same color as the paper color
+    backgroundImage: `url(${'https://www.transparenttextures.com/patterns/cubes.png'})`,
+  },
+  mainContainer:{
+    width:"50%",
+    [theme.breakpoints.down("md")]:{
+      width:"100%"
+    }
+  }
+}));
+
+function App(props) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down('xs')); // Anything size xs we return matches
+  const { darkMode, setDarkMode } = props;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid
+      className={classes.root}
+      container
+      justify='center'
+      alignItems={matches ? 'flex-start' : 'center'}
+      
+    >
+      <Grid item  className={classes.mainContainer}>
+        
+        <FormControlLabel
+          control={
+            <Switch
+              checked={darkMode}
+              onChange={() => setDarkMode(!darkMode)}
+            />
+          }
+          label='Dark Mode'
+        />
+      </Grid>
+    </Grid>
   );
 }
 
-export default App;
+export default withTheme(App);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
+import history from "./history";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import {theme} from "./theme";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+     <Router history={history}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+         
+            <App />
+          </ThemeProvider>
+          </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
