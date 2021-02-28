5e93111a857d08b63ecd819f7a715b1c564e0a4f
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2021-02-28 17:16:56 +0530

    [Added routes

--- src/App.js
+++ src/App.js
@@ -1,62 +1,64 @@
-import React from 'react';
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import { useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
 
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
-
-import { useTheme } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
-
-import { withTheme }  from "./theme"
+import Movie from "./modules/index";
+import { MovieProvider } from "./contexts/MovieContext";
+import { withTheme } from "./theme";
+import NavBar from "./modules/NavBar";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%',
-    height: '100%',
-    [theme.breakpoints.down('xs')]: {
-      paddingTop: theme.spacing(2), // Material UI spacing
-    },
-    position: 'absolute',
-    overflow: 'auto',
     backgroundColor: theme.palette.background.paper, // Makes it the same color as the paper color
-    backgroundImage: `url(${'https://www.transparenttextures.com/patterns/cubes.png'})`,
+    backgroundImage: `url(${"https://www.transparenttextures.com/patterns/cubes.png"})`,
   },
-  mainContainer:{
-    width:"50%",
-    [theme.breakpoints.down("md")]:{
-      width:"100%"
-    }
-  }
+
 }));
 
 function App(props) {
   const classes = useStyles();
   const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.down('xs')); // Anything size xs we return matches
+  const matches = useMediaQuery(theme.breakpoints.down("xs")); // Anything size xs we return matches
   const { darkMode, setDarkMode } = props;
 
   return (
-    <Grid
-      className={classes.root}
-      container
-      justify='center'
-      alignItems={matches ? 'flex-start' : 'center'}
-      
-    >
-      <Grid item  className={classes.mainContainer}>
-        
-        <FormControlLabel
-          control={
-            <Switch
-              checked={darkMode}
-              onChange={() => setDarkMode(!darkMode)}
-            />
-          }
-          label='Dark Mode'
-        />
+    <MovieProvider>
+      <Grid
+        className={classes.root}
+        container
+        justify="center"
+        alignItems={matches ? "flex-start" : "center"}
+        direction="column"
+      >
+        <Grid item container>
+          <NavBar />
+        </Grid>
+        <Grid item container justify="flex-end">
+          <FormControlLabel
+            control={
+              <Switch
+                checked={darkMode}
+                onChange={() => setDarkMode(!darkMode)}
+              />
+            }
+            label="Dark Mode"
+          />
+        </Grid>
+        <Grid item>
+          <Route
+            exact
+            path="/"
+            component={() => <Redirect to="/moviecart" />}
+          />
+          <Route component={Movie} path="/moviecart" />
+        </Grid>
       </Grid>
-    </Grid>
+    </MovieProvider>
   );
 }
 
