b8925dbc21680b6f4ca4e27a25788cd9644276cb
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2021-02-28 18:49:57 +0530

    restructed theme provider

--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,9 @@
 import React from 'react';
-
-import useLocalStorage from './useLocalStorage';
-
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import purple from '@material-ui/core/colors/purple';
 import green from '@material-ui/core/colors/green';
+
+import useLocalStorage from './useLocalStorage';
 
 export const theme = createMuiTheme({
   palette: {
